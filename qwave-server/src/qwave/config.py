e9aa9f7ccebfde89420d8bf6b2530f361b96704d
qwikster <ocoincs@gmail.com>
2026-01-13 03:05:50 -0500

    set up config.py stuffs, need to think about what exactly goes in the config

--- qwave-server/src/qwave/config.py
+++ qwave-server/src/qwave/config.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 from pathlib import Path
 from dataclasses import dataclass
@@ -6,9 +7,41 @@ from typing import Optional
 @dataclass
 class Config:
     database_url: str
-    ...
+    ... # TODO: add all the thingies
 
 _config: Optional[Config] = None
+
+def find_config_file() -> Path:
+    # QWAVE_CONFIG env variable, ./config.yaml, ~/.config/qwave/config.yaml, /etc/qwave/config.yaml
+    return Path.home() # TODO: search through standard locations
+
+def load_config(config_path: Optional[Path] = None) -> Config:
+    global _config
+    
+    if config_path is None:
+        config_path = find_config_file()
+
+    with open(config_path) as f:
+        data = yaml.safe_load(f)
+    
+    def resolve_path(path_str: str) -> Path:
+        path = Path(path_str)
+        if not path.is_absolute():
+            # make relative paths to config file location, should only be true in development or odd clone installs
+            path = (config_path.parent / path).resolve()
+        return path
+    
+    database_path = resolve_path(data["database_path"])
+    music_dir =     resolve_path(data["music_dir"])
+    temp_dir =      resolve_path(data["temp_dir"])
+    
+    # TODO: make sure they're created in the init.py run as well as pick locations
+    _config = Config(
+        ... = ...,
+        ... = ..., 
+    )
+    
+    return _config
 
 def get_config() -> Config:
     if _config is None:
