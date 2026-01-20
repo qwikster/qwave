d5ce3b2be6ed9e30612fedf229d3071c295fd648
qwikster <ocoincs@gmail.com>
2026-01-20 01:04:08 -0500

    app is launching!

--- qwave-server/src/qwave/main.py
+++ qwave-server/src/qwave/main.py
@@ -1,9 +1,51 @@
+import sys
+import uvicorn
+
+from pathlib import Path
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from qwave.config import load_config, get_config
+from qwave.database import init_db, create_tables
+from importlib.metadata import version
+# from qwave.workers.worker import start_worker, stop_worker
+from fastapi.middleware.cors import CORSMiddleware
+
+# from qwave.api import ...
+# TODO: add workers and routers
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # FIXME: add color codes from config and stylize
+    print("Starting server...")
+    # ...
+    yield
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title =       "qWave",
+        description = "Lightweight audio-only media server",
+        version =     version("qwave"),
+        lifespan =    lifespan,
+        docs_url =    "/docs",
+        redoc_url=    "/redoc",
+    )
+    
+    # TODO: CORS, root endpoint, /health, routers
+    
+    return app
 
 def entry():
-    print("test success")
     load_config()
-    print(get_config())
+    app = create_app()
+    config = get_config()
+    
+    uvicorn.run(
+        app,
+        host = config.host,
+        port = config.port,
+        log_level = "info"
+    )
 
 if __name__ == "__main__":
     entry()
