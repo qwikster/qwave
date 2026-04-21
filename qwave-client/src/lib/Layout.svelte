a4793fba35a6bc3bfcd632534b6171fef8c6f121
qwikster <ocoincs@gmail.com>
2026-04-21 02:26:25 -0400

    zed didn't stage these for some reason

---
+++ qwave-client/src/lib/Layout.svelte
@@ -0,0 +1,40 @@
+<script>
+    // bleh
+</script>
+
+<div class = "app">
+    <header>
+        <span class = "logo">qWave</span>
+        <input type = "search" placeholder = "SEARCH..." />
+        <span class = "clock">10:10PM</span>
+    </header>
+    <div class = "main">
+        <aside class = "sidebar">
+            <nav>
+                <a href = null>SONGS</a>
+                <a href = null>ARTISTS</a>
+                <a href = null>ALBUMS</a>
+                <a href = null>PLAYLISTS</a>
+                <a href = null>RECENT</a>
+                <a href = null>RANDOM</a>
+            </nav>
+            <div class = "sidebar-bottom">
+                <button>Upload</button>
+                <button>Settings</button>
+            </div>
+        </aside>
+        <div class = "content">
+            <div class = "top">
+                <div class = "visualizer">music mmm</div>
+                <div class = "now-playing">now playing: balls</div>
+            </div>
+            <div class = "panel">
+                panel here
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+</style>
